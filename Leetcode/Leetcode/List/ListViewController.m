c6e456e22e80ed24bda3ccccff62a443cd2501fa
Liujun <bobzhang@pptv.com>
2019-08-28 10:19:49 +0800

    增加单链表算法

--- Leetcode/Leetcode/List/ListViewController.m
+++ Leetcode/Leetcode/List/ListViewController.m
@@ -27,9 +27,10 @@
     self.afterLabel.textAlignment = NSTextAlignmentCenter;
     [self.view addSubview:self.afterLabel];
     
-    ListNode *listNode = [ListNode creatListNodeForm:@[@1,@1,@2,@2,@3,@3,@4,@4,@7]];
-    self.originalLabel.text = [listNode printAllListNode];
-    self.afterLabel.text = [[self deleteDuplicates2:listNode] printAllListNode];
+    ListNode *listNode1 = [ListNode creatListNodeForm:@[@2,@4,@3,@1]];
+    ListNode *listNode2 = [ListNode creatListNodeForm:@[@5,@6,@4]];
+    self.originalLabel.text = [listNode1 printAllListNode];
+    self.afterLabel.text = [[self addTwoNumber:listNode1 other:listNode2] printAllListNode];
     
 }
 
@@ -113,10 +114,103 @@ Output: 1->1->2->3->4->4
  
 */
 - (ListNode *)mergoTwoList:(ListNode *)l1 other:(ListNode *)l2 {
-    
+    if (l1 == nil) {
+        return l2;
+    }
+    if (l2 == nil) {
+        return l1;
+    }
     ListNode *allList = nil;
+    if (l1.value < l2.value) {
+        allList = l1;
+        l1 = l1.next;
+    } else {
+        allList = l2;
+        l2 = l2.next;
+    }
+    ListNode *p = allList;
+    while (l1 != nil && l2 != nil) {
+        if (l1.value < l2.value) {
+            p.next = l1;
+            l1 = l1.next;
+        } else {
+            p.next = l2;
+            l2 = l2.next;
+        }
+        p = p.next;
+    }
+    
+    if (l1 == nil) {
+        p.next = l2;
+    }
+    if (l2 == nil) {
+        p.next = l1;
+    }
     
     return allList;
+}
+
+/*
+2. Add Two Numbers
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example: Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8 Explanation: 342 + 465 = 807.
+*/
+
+- (ListNode *)addTwoNumber:(ListNode *)l1 other:(ListNode *)l2 {
+    if (l1 == nil) {
+        return l2;
+    }
+    if (l2 == nil) {
+        return l1;
+    }
+    ListNode *p = [[ListNode alloc]init];
+    ListNode *resultList = p;
+    int num = 0; //十进制进位
+    while (l1 != nil || l2 != nil) {
+        int l1Value = 0;
+        if (l1 != nil) {
+            l1Value = (int)l1.value;
+        }
+        int l2Value = 0;
+        if (l2 != nil) {
+            l2Value = (int)l2.value;
+        }
+        int sum = l2Value + l1Value + num;
+        num = sum/10;
+        ListNode *next = [[ListNode alloc]init];
+        next.value = sum%10;
+        p.next = next;
+        p = p.next;
+        l1 = l1.next;
+        l2 = l2.next;
+    }
+    if (num > 0) {
+        ListNode *last = [[ListNode alloc]init];
+        last.value = num;
+        p.next = last;
+    }
+    resultList = resultList.next;
+    return resultList;
+}
+
+/*
+ 206. Reverse Linked List
+ 
+ Reverse a singly linked list.
+ 
+ Example:
+ 
+ Input: 1->2->3->4->5->NULL
+ Output: 5->4->3->2->1->NULL
+ */
+- (ListNode *)reverseLinkedList:(ListNode *)head {
+    ListNode *resultList = [[ListNode alloc]init];
+    
+    return ListNode;
 }
 
 
